3da0053a4532b446dd914ce8654f0cc18d2b542c
TeiderseleshI <redietseleshii@gmail.com>
2025-06-07 11:17:55 +0300

    Made some changes that were causing error.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,7 +4,6 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
@@ -20,10 +19,9 @@ def main():
             converted_temp = convert_to_celsius(temp)
             print(f"{temp}°F is {converted_temp:.2f}°C")
         else:
-            print("Error: Invalid unit. Please enter 'C' or 'F'.")
+            print("Invalid unit. Please enter 'C' or 'F'.")
     except ValueError:
-        print("Error: Invalid input. Please enter a numeric temperature value.")
-
+        print("Invalid temperature. Please enter a numeric value.")
 
 if __name__ == "__main__":
     main()
